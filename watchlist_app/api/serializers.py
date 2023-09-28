663f9b93f6a211fb73281d191fc97b6151eb29d1
anetapec <aneta.pecka@gmail.com>
2023-09-28 10:11:05 +0200

    more options

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -11,4 +11,10 @@ class MovieSerializer(serializers.Serializer):
         return Movie.objects.create(**validated_data)
     
     def update(self, instance, validated_data):
-        instance.
+        instance.name = validated_data.get('name', instance.name)
+        instance.descripition = validated_data.get('description', instance.description)
+        instance.active = validated_data.get('active', instance.active)
+        instance.save()
+        return instance
+        
+        
