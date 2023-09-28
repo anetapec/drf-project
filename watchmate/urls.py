250cd575d9f47dbb9d60c34dfcd56c24f9fbdba3
anetapec <aneta.pecka@gmail.com>
2023-09-28 09:05:13 +0200

    api REST framework

--- watchmate/urls.py
+++ watchmate/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('movie/', include('watchlist_app.urls')),
+    path('movie/', include('watchlist_app.api.urls')),
 ]
