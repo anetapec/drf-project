e25ac947fd50fbf7bd8fb6a2f3b9cd2cd78ad0ef
anetapec <aneta.pecka@gmail.com>
2023-09-30 22:31:46 +0200

    mixins based views

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from watchlist_app.models import WatchList, StreamPlatform
+from watchlist_app.models import WatchList, StreamPlatform, Review
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = "__all__"
+
 
 class WatchSerializer(serializers.ModelSerializer):
+    reviews = ReviewSerializer(many=True, read_only=True)
     # len_name = serializers.SerializerMethodField()
     class Meta:
         model = WatchList
