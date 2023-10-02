25ae0f514cbc889f6fb08617f85177c47f1b77c2
anetapec <aneta.pecka@gmail.com>
2023-10-02 21:03:39 +0200

    wywtawianie ocen wg filmów

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -4,8 +4,8 @@ from watchlist_app.models import WatchList, StreamPlatform, Review
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
-        fields = "__all__"
-
+        #fields = "__all__"
+        exclude = ('watchlist',)
 
 class WatchSerializer(serializers.ModelSerializer):
     reviews = ReviewSerializer(many=True, read_only=True)
